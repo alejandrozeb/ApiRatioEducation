48e81c6f8a7a0716e6467738d1b0c18a3419405c
unknown <alejandrojzeballos@gmail.com>
2022-02-03 18:38:30 -0400

    {add} añadiendo el endpoint /country

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-import express from 'express';
+import express from "express";
 
-const app = express()
+const app = express();
 app.use(express.json());
 
-app.get("/ping", (req,res)=>{
-    res.send("pong");
+app.get("/ping", (req, res) => {
+  res.send("pong");
 });
 
+app.get("/country", (req,res) =>{
+    res.json([]);
+});
 
 export default app;
--- test/index.spec.js
+++ test/index.spec.js
@@ -7,3 +7,15 @@ describe("GET /Ping", () =>{
         expect(response.statusCode).toBe(200);    
     });
 });
+
+describe("Get /country",() =>{
+    test("Should respond with a 200 status code", async()=>{
+        const response = await request(app).get("/country").send();
+        expect(response.statusCode).toBe(200);
+    });
+
+    test("Should respond with a json", async()=>{
+        const response = await request(app).get("/country").send();
+        expect(response.body).toBeInstanceOf(Array);
+    });
+});
