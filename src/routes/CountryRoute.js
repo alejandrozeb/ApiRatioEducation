7718209867abc4d48e7bfb84741ea8140d144fd2
unknown <alejandrojzeballos@gmail.com>
2022-03-03 18:25:36 -0400

    {refac} Country route /all cambiado llamando a controller

--- src/routes/CountryRoute.js
+++ src/routes/CountryRoute.js
@@ -1,8 +1,11 @@
 const {Router}= require("express");
 const router = Router();
+const controllers = require('../controllers/index');
 
-router.get("/all", (req,res)=>{
+/* router.get("/all", (req,res)=>{
     res.json({"Countries": "Bolivia" });
-});
+}); */
+
+router.get("/all", controllers.countryController.allCountries);
 
 module.exports=router;
