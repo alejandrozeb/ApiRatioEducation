1f7cbf6f5fb8faf3c77e849f78a0856f53d67047
unknown <alejandrojzeballos@gmail.com>
2022-03-07 18:17:03 -0400

    {add} añañdiendo unit test a country repository getAll method should be defined

--- __test__/repositories/countryRepository.test.js
+++ __test__/repositories/countryRepository.test.js
@@ -1,9 +1,13 @@
 const repositories = require('../../src/repositories/index');
 
 describe('Country repository', ()=>{
-    test('should be defined',() => {
+    test('Class should be defined',() => {
         const countryRepository = repositories.countryRepository;
         expect(countryRepository).toBeDefined();
     });
-    
+
+    test('get all method should be defined', () => {
+        const countryRepository = repositories.countryRepository.getAll;
+        expect(countryRepository).toBeDefined();
+    });
 });
