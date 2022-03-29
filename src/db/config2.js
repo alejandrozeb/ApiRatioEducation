f72cb013a08560685d66f807fd43ead248f875b3
alejandro zeballos aguilar <alejandrojzeballos@gmail.com>
2022-03-29 19:42:45 -0400

    {add} añadeindo variables de entorno

---
+++ src/db/config2.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv').config();
+
+module.exports = {
+    HOST: process.env.HOST,
+    PORT: process.env.PORT,
+    DBNAME: process.env.DBNAME,
+    USER: process.env.USER,
+    PASSWORD: process.env.PASSWORD,
+};
