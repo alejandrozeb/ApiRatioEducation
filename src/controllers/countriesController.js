e6b2008738fa4f5afa73d657a15364d527e86f6c
alejandro zeballos aguilar <alejandrojzeballos@gmail.com>
2022-04-07 18:31:05 -0400

    {refac} calling getAllcountries method from CountryService in countri controller

--- src/controllers/countriesController.js
+++ src/controllers/countriesController.js
@@ -1,8 +1,9 @@
+const Services = require('../services/index');
 class CountryController{}
 
 CountryController.allCountries = (req,res)=>{
+    let dataCountries = Services.CountryService.getAllCountries();
     res.json({"Countries": "Bolivia" })
 }
-
 
 module.exports = CountryController;
